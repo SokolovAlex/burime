cd25aee2f9ba9309d34114e385e168fa52dcdbfb
sokolov alex <alex.sklv85@gmail.com>
2019-11-21 00:47:36 +0300

    styles for input name

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -1,22 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { socket } from "../../services/socket";
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import { ChatContainer, NameBackground, NameInputWrapper, WhiteTextField, MessagesContainer, NewMessageContainer } from "./styled";
+import { Button } from "@material-ui/core";
+import storage from '../../services/storage';
 
-export const Chat = () =>{
-  const [ name, setName] = useState();
+export const Chat = () => {
+  const [ name, setName] = useState(storage.getItem('name'));
   const [ inputName, setInputName] = useState();
 
   const send = () => {
     socket.emit('sendMessage', '!!!');
   }
 
-  const confirm = () => {
-    setName(inputName)
+  const handleKeyPress = (e: any) => {
+    if (e.key === 'Enter'){
+      setName(inputName);
+      storage.setItem('name', inputName);
+    }
   }
 
   const clearName = () => {
     setName(null)
+    storage.removeItem('name');
   }
 
   useEffect( () => {
@@ -30,25 +35,32 @@ export const Chat = () =>{
           Чат
         </div>
 
+        <ChatContainer>
         { name ?
-            (<div>
-              <div>Ваше имя:{name}</div>
-              <Button onClick={() => clearName()}>другое имя</Button>
-              </div>)
+            (<>
+                <MessagesContainer></MessagesContainer>
+                <NewMessageContainer>
+                  <Button variant="contained" color="primary">Отправить</Button>
+                  <Button variant="contained" onClick={() => clearName()}>Новое имя</Button>
+                </NewMessageContainer>
+              </>)
             :
             (
-              <div>
-                <div>Придумайте себе имя для чата</div>
-                <TextField
-                  label="имя"
-                  type="text"
-                  margin="normal"
-                  onChange={(e) => setInputName(e.target.value)}
-                />
-                <Button onClick={() => confirm()}>ok</Button>
-              </div>
+              <NameBackground>
+                <NameInputWrapper>
+                  <WhiteTextField
+                    label="Имя для чата"
+                    variant="outlined"
+                    type="text"
+                    margin="normal"
+                    onKeyPress={(e) => handleKeyPress(e)}
+                    onChange={(e) => setInputName(e.target.value)}
+                  />
+                </NameInputWrapper>
+              </NameBackground>
             )
         }
+        </ChatContainer>
       </div>
     )
 }
---
+++ services/storage.ts
@@ -0,0 +1,36 @@
+class Storage {
+  instanse: any = null;
+  
+  constructor() {
+    if (typeof window === "undefined") {
+      this.instanse = {
+        getItem: () => {},
+        setItem: () => {},
+        removeItem: () => {},
+      };
+      return;
+    }
+    this.instanse = window.localStorage;
+  }
+
+  getItem(key: string) {
+    const value = this.instanse.getItem(key);
+    let result;
+    try {
+      result = JSON.parse(value);
+    } catch {
+      result = value;
+    }
+    return result;
+  }
+
+  setItem(key: string, value: any) {
+    this.instanse.setItem(key, JSON.stringify(value));
+  }
+
+  removeItem(key: string) {
+    this.instanse.removeItem(key);
+  }
+}
+
+export default new Storage();
