7348ad02994f705938e9dca844afdb03dd0dc070
alex sokolov <alex.sklv85@gmail.com>
2020-05-20 23:58:27 +0300

    return domail to localhost

--- server/config.ts
+++ server/config.ts
@@ -1,5 +1,5 @@
 export const isDev = process.env.NODE_ENV !== 'prod';
-export const domain = isDev ? 'localhost' : '0.0.0.0';
+export const domain ='localhost';
 export const clientUrl = isDev ? 'http://localhost:3002' : 'http://goburime.ru';
 export const serverUrl = isDev ? 'http://localhost:3003' : 'http://goburime.ru:3003';
 
